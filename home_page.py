9445e2d8cd3cc6a0714280a3a44a424ad27409cc
Bookuh <131380823+Bookuh@users.noreply.github.com>
2023-06-09 13:58:57 -0500

    Initial commit

---
+++ home_page.py
@@ -0,0 +1,29 @@
+import streamlit_pandas as sp
+import pandas as pd
+import streamlit as st
+
+@st.cache(allow_output_mutation=True)
+
+def pipeline(filepath):
+    dataframe = pd.read_csv(filepath)
+    return dataframe
+
+df = pipeline(r'C:\Users\willi\OneDrive\Desktop\Data\Coding_Temple_Week_6\titanic_cleaned.csv')
+
+st.title('Titanic Data')
+st.text("Here I've built an application where you can filter throught the Titanic data set using the buttons on the side of the page." )
+
+
+features = {
+
+    'sex': 'multiselect',
+    'embarked': 'select',
+    'survived': 'multiselect',
+    'p_class': 'multiselect',
+    'sib_sp': 'multiselect',
+    'parch': 'multiselect'
+}
+
+all_widgets = sp.create_widgets(df, features, ignore_columns=['passanger_id'])
+res = sp.filter_df(df, all_widgets)
+st.write(res)
