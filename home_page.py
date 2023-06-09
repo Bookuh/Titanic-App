30891b6a32ad12b55fc4aad502d7c648d2a6b980
Bookuh <131380823+Bookuh@users.noreply.github.com>
2023-06-09 14:44:29 -0500

    Update home_page.py

--- home_page.py
+++ home_page.py
@@ -1,15 +1,14 @@
 import streamlit_pandas as sp
 import pandas as pd
 import streamlit as st
-from pathlib import Path
-file = Path(__file__).parent/ 'C:/Users/willi/OneDrive/Desktop/Data/Coding_Temple_Week_6/titanic_cleaned.csv' 
+
 @st.cache(allow_output_mutation=True)
 
 def pipeline(filepath):
     dataframe = pd.read_csv(filepath)
     return dataframe
 
-df = pipeline(file)
+df = pipeline(r'C:/Users/willi/OneDrive/Desktop/Data/Coding_Temple_Week_6/titanic_cleaned.csv')
 
 st.title('Titanic Data')
 st.text("Here I've built an application where you can filter throught the Titanic data set using the buttons on the side of the page." )
